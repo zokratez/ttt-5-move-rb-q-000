2d587aa075de0b65c2de2567a12d055b096f56f8
zokratez <tortillabar@me.com>
2016-01-12 18:40:11 +0000

    nitrous-backup at Tue Jan 12 18:40:11 UTC 2016

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,5 @@
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +9,7 @@ def display_board(board)
 end
 
 # code your move method here!
+def move(board, location, charac = "X")
+  board [location - 1] = charac
+
+end
